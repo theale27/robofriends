b2c3e93652020bd3ed5fbaa4c2ae936611e06c4b
Theale <tgghastelow@gmail.com>
2019-03-12 15:51:04 +1000

    Updates

---
+++ precache-manifest.3b95beba1ffd0f662babbc05519d36ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "2df6d3d367dcc46a6c72",
+    "url": "/robofriends/static/js/main.2df6d3d3.chunk.js"
+  },
+  {
+    "revision": "e1bec069cdbcd02c65e5",
+    "url": "/robofriends/static/js/2.e1bec069.chunk.js"
+  },
+  {
+    "revision": "2df6d3d367dcc46a6c72",
+    "url": "/robofriends/static/css/main.ba418765.chunk.css"
+  },
+  {
+    "revision": "e1bec069cdbcd02c65e5",
+    "url": "/robofriends/static/css/2.ad20f552.chunk.css"
+  },
+  {
+    "revision": "d3f792ecad443a860b92e0c4a8198360",
+    "url": "/robofriends/index.html"
+  }
+];
